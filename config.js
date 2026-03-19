d28c7e01cfee02dc47258bf7a9af8e299ed18c66
cutemiki20-lgtm <cutemiki2.0@gmail.com>
2026-03-19 14:41:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  id: isNaN(parsedId) ? 123456789 : parsedId // replace 12345.. with your telegram chat id
+  token: process.env.TOKEN || '8799759537:AAEo__Kwpun4Wy4doKRcsDcttdqzrY1qaRg',
+  id: isNaN(parsedId) ? 2081362297 : parsedId // replace 12345.. with your telegram chat id
 };
